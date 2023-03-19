8cafa0cf9fa6ca401f0920aee64730ec74427d95
JeongukChoi <onsucjuk@gmail.com>
2023-03-19 10:12:03 +0900

    bj10871

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,28 @@
-
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
+     Scanner sc = new Scanner(System.in);
 
+     int a = sc.nextInt();
+     int b = sc.nextInt();
+
+
+
+     for (int i = 0; i<a; i++) {
+
+         int list[] = new int[a];
+
+         list[i] = sc.nextInt();
+
+         if (list[i]<b) {
+
+             System.out.print(list[i]+" ");
+
+         }
+
+     }
 
     }
-
 
 }
