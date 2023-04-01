3c346d9007ff05b49df1cdf988b32f200138477c
JeongukChoi <onsucjuk@gmail.com>
2023-04-01 09:30:26 +0900

    Ex 11654

---
+++ src/Ex_11654.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Ex_11654 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.next().charAt(0);
+
+        System.out.println(a);
+
+
+    }
+}
